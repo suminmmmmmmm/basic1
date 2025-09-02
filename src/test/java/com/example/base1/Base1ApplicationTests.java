295a3cb95d6cd0db1ccfeecb8aaf5a0d5707ba06
suminmmmmmmm <gims07737@gmail.com>
2025-09-02 19:01:57 +0900

    기초세팅

---
+++ src/test/java/com/example/base1/Base1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.base1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
